a1fd08f4033b11e644aebd740b9e1044ad6df215
Αγγελική Κουρνέτα <43947873+p17kour@users.noreply.github.com>
2018-11-05 17:19:48 +0200

    Update mouseover.js

--- mouseover.js
+++ mouseover.js
@@ -1,14 +1,13 @@
 function playclip() 
 {
-  if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) 
-  {
-    if (document.all)
+   if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) 
+   {
+       if (document.all)
+          document.all.sound.src = "click.mp3";
+    } 
+    else 
     {
-      document.all.sound.src="click.mp3";
+        var audio = document.getElementsByTagName("audio")[0];
+        audio.play();
     }
-  }else 
-  {
-    var audio = document.getElementsByTagName("audio")[0];
-    audio.play();
-  }
 }
